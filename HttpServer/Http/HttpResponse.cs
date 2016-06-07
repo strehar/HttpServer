7b0c438b1c1c6d3266927d6fcc65b45df6d23821
miha <MS>
2016-06-07 11:47:20 +0200

    Release 0.2.0.2

--- HttpServer/Http/HttpResponse.cs
+++ HttpServer/Http/HttpResponse.cs
@@ -190,7 +190,7 @@ namespace Feri.MS.Http
             catch (Exception e)
             {
                 // Something went wrong with request processing and connection is not open or connection was closed by teh client before we could send a response.
-                System.Diagnostics.Debug.WriteLine("Connection was closed in Write() method (" + e.Message + ").");
+                System.Diagnostics.Debug.WriteLineIf(_debug, "Connection was closed in Write() method (" + e.Message + ").");
             }
         }
 
